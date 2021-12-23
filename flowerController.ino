4a6aa9c133d25e753072096d285af61aa4cc0eac
jdurham38 <60802210+jdurham38@users.noreply.github.com>
2021-12-22 19:24:28 -0500

    Create flowerController.ino

---
+++ flowerController.ino
@@ -0,0 +1,62 @@
+/*  
+   Arduino PIR controller motion sensor for mechanical Flower
+*/
+include Servo
+ 
+Servo servo_test;    		//initialize a servo object for the connected servo  
+                
+int angle = 0;    
+int led = 13;                // the pin that the LED is atteched to
+int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
+  servo_test.attach(9); 		 // attach the signal pin of servo to pin9 of arduino
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    delay(100);                // delay 100 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      delay(200);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+  
+  //servo
+  for(angle = 0; angle < 180; angle += 1) 	 // command to move from 0 degrees to 180 degrees 
+  {                                  
+    servo_test.write(angle);              	 //command to rotate the servo to the specified angle
+    delay(15);                       
+  } 
+ 
+  delay(1000);
+  
+  for(angle = 180; angle>=1; angle-=5)     // command to move from 180 degrees to 0 degrees 
+  {                                
+    servo_test.write(angle);              //command to rotate the servo to the specified angle
+    delay(5);                       
+  } 
+
+    delay(1000);
+}
+
+
+
+
